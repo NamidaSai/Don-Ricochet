63bfde9cb4dea1c506cc9f3a3d335eb717c83abc
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-05-27 15:40:01 +0100

    Basic Player movement implemented with animations from asset pack.

---
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform target = default;
+
+    void LateUpdate()
+    {
+        transform.position = target.position;
+        
+    }
+}
