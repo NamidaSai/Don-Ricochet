922a351081a76215bb969f1686259923a8d8c647
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-05-27 17:44:25 +0100

    Added namespaces and dependencies.

--- Assets/Scripts/Core/FollowCamera.cs
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCamera : MonoBehaviour
+namespace RPG.Core
 {
-    [SerializeField] Transform target = default;
-
-    void LateUpdate()
+    public class FollowCamera : MonoBehaviour
     {
-        transform.position = target.position;
+        [SerializeField] Transform target = default;
+
+        void LateUpdate()
+        {
+            transform.position = target.position;
+        }
     }
 }
