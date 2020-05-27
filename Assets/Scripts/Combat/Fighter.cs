8dca7915c3fa431f81ca788fb99b4010de03efda
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-05-27 21:38:50 +0100

    Avoided dependency cycle with interface + Start of Attack Behaviour.

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,14 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.Movement;
+using RPG.Core;
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour
+    public class Fighter : MonoBehaviour, IAction
     {
-        public void Attack(CombatTarget target)
+        [SerializeField] float weaponRange = 2f;
+
+        Transform target;
+
+        private void Update()
         {
-            print("Take that, giant!");
+            if (target == null) { return; }
+
+            if (!GetIsInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            GetComponent<Animator>().SetTrigger("attack");
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(target.position, transform.position) < weaponRange;
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            target = combatTarget.transform;
+        }
+
+        public void Cancel()
+        {
+            target = null;
+        }
+
+        // Animation Event
+        void Hit()
+        {
+            
         }
     }
 }
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -9,7 +9,7 @@ namespace RPG.Control
     public class PlayerController : MonoBehaviour
     {
 
-        void Update()
+        private void Update()
         {
             if (InteractWithCombat()) { return; }
             if (InteractWithMovement()){ return; }
@@ -40,7 +40,7 @@ namespace RPG.Control
             {
                 if (Input.GetMouseButton(0))
                 {
-                    GetComponent<Mover>().MoveTo(hit.point);
+                    GetComponent<Mover>().StartMoveAction(hit.point);
                 }
                 return true;
             }
