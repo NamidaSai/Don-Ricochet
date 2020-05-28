d93add75af04cb1df1f21ac3733932192ab0afff
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-05-28 14:10:20 +0100

    Disable control on Death + AI Guard Behaviour + Gizmos

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -13,7 +13,7 @@ namespace RPG.Combat
         [SerializeField] float weaponDamage = 20f;
 
         Health target;
-        float timeSinceLastAttack = 0f;
+        float timeSinceLastAttack = Mathf.Infinity;
 
         private void Update()
         {
@@ -61,14 +61,14 @@ namespace RPG.Combat
             return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
         }
 
-        public bool CanAttack(CombatTarget combatTarget)
+        public bool CanAttack(GameObject combatTarget)
         {
             if (combatTarget == null) { return false; }
             Health targetToTest = combatTarget.GetComponent<Health>();
             return targetToTest != null && !targetToTest.IsDead();
         }
 
-        public void Attack(CombatTarget combatTarget)
+        public void Attack(GameObject combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.GetComponent<Health>();
---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Combat;
+using RPG.Movement;
+
+namespace RPG.Core
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 10f;
+
+        Fighter fighter;
+        Mover mover;
+        Health health;
+        GameObject player;
+
+        Vector3 guardPosition;
+
+        private void Start()
+        {
+            fighter = GetComponent<Fighter>();
+            mover = GetComponent<Mover>();
+            health = GetComponent<Health>();
+            player = GameObject.FindWithTag("Player");
+
+            guardPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            if (health.IsDead()) { return; }
+
+            if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
+            {
+                fighter.Attack(player);
+            }
+            else
+            {
+                mover.StartMoveAction(guardPosition);
+            }
+        }
+
+        private bool InAttackRangeOfPlayer()
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            return distanceToPlayer < chaseDistance;
+        }
+
+        // Called by Unity
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
+    }
+}
