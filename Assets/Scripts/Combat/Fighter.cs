0c143de0d8dcbbd0563cfdeca28880731a2d9750
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-05-28 11:44:40 +0100

    Death mechanic + idle swap + rotate towards attack target

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -10,18 +10,20 @@ namespace RPG.Combat
     {
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 20f;
 
-        Transform target;
+        Health target;
         float timeSinceLastAttack = 0f;
 
         private void Update()
         {
             timeSinceLastAttack += Time.deltaTime;
             if (target == null) { return; }
+            if (target.IsDead()) { return; }
 
             if (!GetIsInRange())
             {
-                GetComponent<Mover>().MoveTo(target.position);
+                GetComponent<Mover>().MoveTo(target.transform.position);
             }
             else
             {
@@ -32,33 +34,37 @@ namespace RPG.Combat
 
         private void AttackBehaviour()
         {
+            transform.LookAt(target.transform);
             if (timeSinceLastAttack > timeBetweenAttacks)
             {
+                // This will trigger the Hit() event.
                 GetComponent<Animator>().SetTrigger("attack");
                 timeSinceLastAttack = 0f;
             }
         }
 
+        // Animation Event
+        void Hit()
+        {
+            target.TakeDamage(weaponDamage);
+        }
+
         private bool GetIsInRange()
         {
-            return Vector3.Distance(target.position, transform.position) < weaponRange;
+            return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
         }
 
         public void Attack(CombatTarget combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
-            target = combatTarget.transform;
+            target = combatTarget.GetComponent<Health>();
         }
 
         public void Cancel()
         {
+            GetComponent<Animator>().SetTrigger("stopAttack");
             target = null;
         }
 
-        // Animation Event
-        void Hit()
-        {
-            
-        }
     }
 }
