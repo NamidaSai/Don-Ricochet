f6600e601e2195d1cb0dcc0f7b00470f8dfb9177
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-06-01 13:46:13 +0100

    Added character rotation save + scene load on play in editor.

--- Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -7,7 +7,17 @@ namespace RPG.SceneManagement
 {
     public class SavingWrapper : MonoBehaviour
     {
+        [SerializeField] float fadeInTime = 0.2f;
+
         const string defaultSaveFile = "save";
+
+        IEnumerator Start()
+        {
+            Fader fader = FindObjectOfType<Fader>();
+            fader.FadeOutImmediate();
+            yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+            yield return fader.FadeIn(fadeInTime);
+        }
 
         void Update()
         {
@@ -21,12 +31,12 @@ namespace RPG.SceneManagement
             }
         }
 
-        private void Load()
+        public void Load()
         {
             GetComponent<SavingSystem>().Load(defaultSaveFile);
         }
 
-        private void Save()
+        public void Save()
         {
             GetComponent<SavingSystem>().Save(defaultSaveFile);
         }
