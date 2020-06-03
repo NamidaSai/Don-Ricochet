3ab373b1cb7fa07f0e2f31ef3c1a3bb4bd5cc527
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-06-03 11:46:25 +0100

    Added projectile for bow + instantiating projectile

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Core;
+
+namespace RPG.Combat
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] float speed = 1f;
+
+        Health target = null;
+
+        private void Update()
+        {
+            if (target == null) { return; }
+            transform.LookAt(GetAimLocation());
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        public void SetTarget(Health target)
+        {
+            this.target = target;
+        }
+
+        private Vector3 GetAimLocation()
+        {
+            CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+
+            if (targetCapsule == null)
+            {
+                return target.transform.position;
+            }
+
+            return target.transform.position + Vector3.up * targetCapsule.height / 2;
+        }
+    }
+}
