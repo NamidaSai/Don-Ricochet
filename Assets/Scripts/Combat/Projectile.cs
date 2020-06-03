47a44eee14f97b04e2db90fe5873586052aba63a
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-06-03 12:00:02 +0100

    Arrow projectile now inflicts damage to target

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -10,6 +10,7 @@ namespace RPG.Combat
         [SerializeField] float speed = 1f;
 
         Health target = null;
+        float damage = 0f;
 
         private void Update()
         {
@@ -18,9 +19,10 @@ namespace RPG.Combat
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
 
-        public void SetTarget(Health target)
+        public void SetTarget(Health target, float damage)
         {
             this.target = target;
+            this.damage = damage;
         }
 
         private Vector3 GetAimLocation()
@@ -33,6 +35,13 @@ namespace RPG.Combat
             }
 
             return target.transform.position + Vector3.up * targetCapsule.height / 2;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Health>() != target) { return; }
+            target.TakeDamage(damage);
+            Destroy(gameObject);
         }
     }
 }
