fd80f189e8cd526e9296d8252f6d061272bf724e
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-06-05 12:34:28 +0100

    Debug enemy target health display tool

---
+++ Assets/Scripts/Combat/EnemyHealthDisplay.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using RPG.Resources;
+
+namespace RPG.Combat
+{
+    public class EnemyHealthDisplay : MonoBehaviour
+    {
+        Fighter fighter;
+
+        void Awake()
+        {
+            fighter = GameObject.FindWithTag("Player").GetComponent<Fighter>();
+        }
+
+        void Update()
+        {
+            if (fighter.GetTarget() == null)
+            {
+                GetComponent<Text>().text = "N/A";
+            }
+            else
+            {
+                Health health = fighter.GetTarget();
+                GetComponent<Text>().text = String.Format("{0:0}%", health.GetPercentage());
+            }
+        }
+    }
+}
