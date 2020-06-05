093bd695d36f9d59c494afa6c731e61c9fbaa4b8
Geoffrey Mugford <geoffrey.mugford@gmail.com>
2020-06-05 15:38:17 +0100

    Added XP debug display

--- Assets/Scripts/Resources/Experience.cs
+++ Assets/Scripts/Resources/Experience.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Resources
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, ISaveable
     {
         [SerializeField] float experiencePoints = 0f;
 
         public void GainExperience(float experience)
         {
             experiencePoints += experience;
+        }
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float)state;
+        }
+
+        public float GetPoints()
+        {
+            return experiencePoints;
         }
     }
 }
---
+++ Assets/Scripts/Resources/ExperienceDisplay.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+namespace RPG.Resources
+{
+    public class ExperienceDisplay : MonoBehaviour
+    {
+        Experience experience;
+
+        void Awake()
+        {
+            experience = GameObject.FindWithTag("Player").GetComponent<Experience>();
+        }
+
+        void Update()
+        {
+            GetComponent<Text>().text = String.Format("{0:0}", experience.GetPoints());
+        }
+    }
+}
